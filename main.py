158cef2f106cff820a09968eb494ab96453ee9ba
K-Alex-N <kurochkinalexei@yandex.ru>
2023-03-29 10:45:16 +0300

    init

---
+++ main.py
@@ -0,0 +1,19 @@
+
+# создаем проект
+# копируем файлы (config.py, config.yaml, .gitignore, main.py, requirements.txt + создаем папку app)
+x = input("Введите название папки в которую скопировать файлы: ")
+# os.mkdir('app')
+
+
+
+#
+#
+#
+#
+#
+#
+#
+#
+
+
+
