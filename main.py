285017c619f6256ef3f0eefa796b6efb95da8aa9
K-Alex-N <kurochkinalexei@yandex.ru>
2023-03-29 11:58:11 +0300

    init

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
+import os
+import shutil
 
 # создаем проект
-# копируем файлы (config.py, config.yaml, .gitignore, main.py, requirements.txt + создаем папку app)
-x = input("Введите название папки в которую скопировать файлы: ")
-# os.mkdir('app')
+  # как создавать проект чтобы он правильно создвался на гитхаб?
 
+# копируем файлы/папки (из папки template_for_new_project) в корень нового проекта
 
+path_for_pycharm_folders = os.path.dirname(os.path.abspath(os.curdir))
+new_project_dir_name = input("Введите название папки нового проекта: ")
+folder_to = os.path.join(path_for_pycharm_folders, new_project_dir_name)
+folder_from = os.path.join(os.path.abspath(os.curdir), 'template_for_new_project')
 
-#
-#
-#
-#
-#
-#
-#
-#
+for f in os.listdir(folder_from):
+    if os.path.isfile(os.path.join(folder_from, f)):
+        shutil.copy(os.path.join(folder_from, f), os.path.join(folder_to, folder_user, f))
+    if os.path.isdir(os.path.join(folder_from, f)):
+        shutil.copytree(os.path.join(folder_from, f), os.path.join(folder_to, folder_user, f))
+
 
 
 
