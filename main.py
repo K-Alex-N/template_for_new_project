ab529bc37e19599a0bc5656b237a0ad361d3e790
K-Alex-N <kurochkinalexei@yandex.ru>
2023-03-30 09:47:25 +0300

    init

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import shutil
 # создаем проект
   # как создавать проект чтобы он правильно создвался на гитхаб?
 
-# копируем файлы/папки (из папки template_for_new_project) в корень нового проекта
+# копируем файлы/папки (из папки app) в корень нового проекта
 
 path_for_pycharm_folders = os.path.dirname(os.path.abspath(os.curdir))
 new_project_dir_name = input("Введите название папки нового проекта: ")
 folder_to = os.path.join(path_for_pycharm_folders, new_project_dir_name)
-folder_from = os.path.join(os.path.abspath(os.curdir), 'template_for_new_project')
+folder_from = os.path.join(os.path.abspath(os.curdir), 'app')
 
 for f in os.listdir(folder_from):
     if os.path.isfile(os.path.join(folder_from, f)):
@@ -17,9 +17,8 @@ for f in os.listdir(folder_from):
     else:
         shutil.copytree(os.path.join(folder_from, f), os.path.join(folder_to, f))
 
+# ________Закрываем текущий проект и переходим в файл main.py в новом проекте_______________
 
-# может быть сразу установку пакетов сделать?
-# пока просто копированием из файла pip_install.txt делать
 
 
 
